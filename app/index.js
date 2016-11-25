cf6170c413b550825618da74e45fdd9bbac4bb02
gjermundnor <gjermundnor@outlook.com>
2016-11-25 22:08:30 +0100

    Basic webpack es6 setup

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// Include components
+import App from './components/App';
+
+render(
+	<App />,
+	document.getElementById('root')
+);
